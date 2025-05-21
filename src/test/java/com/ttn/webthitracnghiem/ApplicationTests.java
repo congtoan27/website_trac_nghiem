5174e3f6ca9b3bee14a6ea1f5ad83a19abfd2c18
congtoan <toan5728@gmail.com>
2025-05-21 13:39:15 +0700

    baseOfProject

---
+++ src/test/java/com/ttn/webthitracnghiem/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn.webthitracnghiem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
